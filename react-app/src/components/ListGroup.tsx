67a10c3a558f2235f3950260b9bab056a1311c22
Adam <akmoore99@gmail.com>
2023-11-18 09:18:58 -0500

    Add state to ListGroup <li>'s to show active

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,24 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "Cairo", "London", "Paris", "Your Mom's House"];
-
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>My List ({items.length})</h1>
       <ul className="list-group">
         {items.map((item, index) => (
-          <li key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
